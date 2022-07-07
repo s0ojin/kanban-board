b7067b455759ea99f8bcde4bce0a3cdf019ce6a8
s0ojin <onepst@hanyang.ac.kr>
2022-07-08 01:25:36 +0900

    deploy

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { RecoilRoot, useRecoilValue } from "recoil";
+import { RecoilRoot } from "recoil";
 import App from "./App";
 
 
